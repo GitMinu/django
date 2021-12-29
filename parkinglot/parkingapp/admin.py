cff827eccab07ca0106c3a31c053b6b1ec00ae8c
GitMinu <annaminnaxav@gmail.com>
2021-12-29 08:22:39 -0700

    Adding Models to the project

--- parkinglot/parkingapp/admin.py
+++ parkinglot/parkingapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Parkingslot, ParkingLot, VehicleDetails
 
 # Register your models here.
+
+class ParkingLotAdmin(admin.ModelAdmin):
+    pass
+
+class ParkingSlotAdmin(admin.ModelAdmin):
+    pass
+
+class VehicleDetailsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ParkingLot, ParkingLotAdmin)
+admin.site.register(Parkingslot, ParkingSlotAdmin)
+admin.site.register(VehicleDetails, VehicleDetailsAdmin)
--- parkinglot/parkingapp/models.py
+++ parkinglot/parkingapp/models.py
@@ -1,3 +1,31 @@
+from typing import TYPE_CHECKING
 from django.db import models
 
 # Create your models here.
+
+class ParkingLot(models.Model):
+    no_of_car_slots = models.IntegerField()
+    no_of_bike_slots = models.IntegerField()
+
+    class Meta:
+        db_table = 'parking_lot'
+
+class Parkingslot(models.Model):
+    TYPE_CHOICES = [('car', 'Car'), ('bike', 'Bike')]
+    slot_no = models.IntegerField()
+    slot_type = models.CharField(choices=TYPE_CHOICES, max_length=10)
+    is_occupied = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'parking_slot'
+
+class VehicleDetails(models.Model):
+    TYPE_CHOICES = [('car', 'Car'), ('bike', 'Bike')]
+    vehicle_no = models.CharField(max_length=10)
+    vehicle_type = models.CharField(choices=TYPE_CHOICES, max_length=10)
+    start_tume = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True)
+    slot = models.ForeignKey(Parkingslot, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'vehicle_details'
